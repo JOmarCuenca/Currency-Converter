b46c4364b02f0ebf478f6d4917615f9c7cb313f4
Jesus Omar <jesomar.cuenca@gmail.com>
2021-07-01 19:15:16 -0500

    Fixed UI for the Currency Picker Dialog

--- lib/models/Dialogs/currencyPicker.dart
+++ lib/models/Dialogs/currencyPicker.dart
@@ -1,23 +1,20 @@
+import 'package:currency_converter/constants/constants.dart';
 import 'package:currency_converter/controllers/currencyPickers.dart';
 import 'package:currency_picker/currency_picker.dart';
 import 'package:flutter/material.dart';
 
-Future<Map<String,Currency>?> pickCurrencyDialog(BuildContext context){
-  return showDialog(
-    context: context, 
-    builder: (c) => CurrencyPickerView()
-  );
+Future<Map<String, Currency>?> pickCurrencyDialog(BuildContext context) {
+  return showDialog(context: context, builder: (c) => CurrencyPickerView());
 }
 
 class CurrencyPickerView extends StatefulWidget {
-  const CurrencyPickerView({ Key? key }) : super(key: key);
+  const CurrencyPickerView({Key? key}) : super(key: key);
 
   @override
   _CurrencyPickerViewState createState() => _CurrencyPickerViewState();
 }
 
 class _CurrencyPickerViewState extends State<CurrencyPickerView> {
-
   late CurrencyPickerController _controller;
 
   @override
@@ -28,58 +25,57 @@ class _CurrencyPickerViewState extends State<CurrencyPickerView> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(    
-      backgroundColor: Colors.transparent,  
-      child: Container(
-        constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height * 22/50
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.blueGrey.withOpacity(0.8)
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              this._buildGD(
-                true, 
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("From:"),
-                    Container(
-                      child: Text(this._controller.fromTextCurrency),
-                    )
-                  ],
-                )
-              ),
-              this._buildGD(
-                false, 
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("To:"),
-                    Container(
-                      child: Text(this._controller.toTextCurrency),
-                    )
-                  ],
-                )
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: ElevatedButton(
-                  onPressed: () => Navigator.of(context).pop(this._controller.formattedValues),
-                  child: Text("Add")
-                ),
-              )
-            ],
-          ),
+    final size = MediaQuery.of(context).size;
+    // ignore: non_constant_identifier_names
+    final SPACING = SizedBox(height: size.height / 24,);
+    return Dialog(
+      shape: DIALOG_STYLE,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            this._buildGD(true, this._buildCurrencyField(context, true)),
+            SPACING,
+            this._buildGD(false, this._buildCurrencyField(context, false)),
+            SPACING,
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                  style: BUTTON_STYLE,
+                  onPressed: () => Navigator.of(context)
+                      .pop(this._controller.formattedValues),
+                  child: Text("Add")),
+            )
+          ],
         ),
       ),
     );
   }
+
+  Widget _buildCurrencyField(BuildContext context, bool from) => Container(
+    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+    decoration: BoxDecoration(
+      border: Border.all(
+        width: 2,
+        color: PRIMARY_COLOR
+      ),
+      borderRadius: BorderRadius.circular(16)
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(from ? "From:" : "To:"),
+        Container(
+          child: from
+              ? Text(this._controller.fromTextCurrency)
+              : Text(this._controller.toTextCurrency),
+        )
+      ],
+    ),
+  );
 
   GestureDetector _buildGD(bool from, Widget child) => GestureDetector(
     child: child,
